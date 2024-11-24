1900522748c4be9efab8f61bdc0612591f1f45ac
Guido Fioravantti <guido.fioravantti@gmail.com>
2024-11-24 11:26:50 -0500

    Remove LESS and Tailwind CSS... use builtin SCSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import './styles/styles.less';
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Saferlayer",
-  description: "Add watermarks to your documents for free and avoid scams",
-};
+import './styles/styles.scss';
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-const withLess = require('next-with-less');
-
-module.exports = withLess({
-    lessLoaderOptions: {
-        // LESS loader options can be specified here
-    },
-});
